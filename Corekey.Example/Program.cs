9164d68aec5db750561ea4559c968c9d51404f60
marihachi <marihachi0620@gmail.com>
2020-06-14 00:01:30 +0900

    imple

--- Corekey.Example/Program.cs
+++ Corekey.Example/Program.cs
@@ -11,9 +11,23 @@ namespace Corekey.Example
 		{
 			Console.WriteLine("A example project of Corekey");
 
+			// create an app
 			var app = await App.Create("misskey.io", "Corekey.Example", "A test app", new[] { "write:notes" });
-			Console.WriteLine($"app host: {app.Host}");
-			Console.WriteLine($"app secret: {app.Secret}");
+
+			// generate an auth session
+			var session = await AuthSession.Generate(app);
+			Console.WriteLine($"session url: {session.Url}");
+
+			// wait the authorization
+			Console.WriteLine("Waiting authorization is complete ...");
+			var account = await session.WaitForAuth();
+
+			// post a note
+			var res = await account.Request("notes/create", new [] {
+				new KeyValuePair<string, object>("text", "Corekey.NET example")
+			});
+			Console.WriteLine(res.ToString());
 		}
 	}
 }
+
--- Corekey/Corekey.cs
+++ Corekey/Corekey.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -32,25 +33,25 @@ namespace Corekey
 			}
 			else
 			{
-				Debug.WriteLine($"-- Begin Request --");
+				//Debug.WriteLine($"-- Begin Request --");
 				var paramDictionary = parameters.ToDictionary(w => w.Key, w => w.Value);
 				var json = JToken.FromObject(paramDictionary).ToString(Formatting.None);
-				Debug.WriteLine($"request data: {json}");
+				//Debug.WriteLine($"request data: {json}");
 				var content = new StringContent(json, Encoding.UTF8, "application/json");
-				Debug.WriteLine($"url: https://{host}/api/{endpoint}");
+				//Debug.WriteLine($"url: https://{host}/api/{endpoint}");
 				var res = await this.Client.PostAsync($"https://{host}/api/{endpoint}", content);
-				Debug.WriteLine($"status: {res.StatusCode}");
+				//Debug.WriteLine($"status: {res.StatusCode}");
 				var resData = await res.Content.ReadAsStringAsync();
-				Debug.WriteLine($"response data: {resData}");
+				//Debug.WriteLine($"response data: {resData}");
 				var parsedToken = JToken.Parse(resData);
-				Debug.WriteLine($"parsed token: {parsedToken}");
-				Debug.WriteLine($"-- End Request --");
+				//Debug.WriteLine($"parsed token: {parsedToken}");
+				//Debug.WriteLine($"-- End Request --");
 				return parsedToken;
 			}
 		}
 	}
 
-	public static class Configurator
+	public static class Configuration
 	{
 		public static IRequester Requester = new HttpRequester(new HttpClient());
 	}
@@ -68,10 +69,10 @@ namespace Corekey
 
 		public static async Task<App> Create(string host, string name, string description, IEnumerable<string> permissions, string callbackUrl = null)
 		{
-			var res = await Configurator.Requester.Request(
+			var res = await Configuration.Requester.Request(
 				host,
 				"app/create",
-				new List<KeyValuePair<string, object>> {
+				new[] {
 					new KeyValuePair<string, object>("name", name),
 					new KeyValuePair<string, object>("description", description),
 					new KeyValuePair<string, object>("permission", permissions),
@@ -80,6 +81,98 @@ namespace Corekey
 				false);
 
 			return new App(host, res.Value<string>("secret"));
+		}
+	}
+
+	public class UserToken
+	{
+		public string AccessToken { get; set; }
+		public JObject User { get; set; }
+
+		public UserToken(string accessToken, JObject user)
+		{
+			this.AccessToken = accessToken;
+			this.User = user;
+		}
+	}
+
+	public class AuthSession
+	{
+		private App App;
+		private string Token;
+		public string Url { get; private set; }
+		private bool Cancel;
+
+		public AuthSession(App app, string token, string url)
+		{
+			this.App = app;
+			this.Token = token;
+			this.Url = url;
+			this.Cancel = false;
+		}
+
+		public static async Task<AuthSession> Generate(App app)
+		{
+			var authSession = await Configuration.Requester.Request(app.Host, "auth/session/generate", new[] {
+				new KeyValuePair<string, object>("appSecret", app.Secret)
+			}, false);
+			return new AuthSession(app, authSession.Value<string>("token"), authSession.Value<string>("url"));
+		}
+
+		public async Task<UserToken> GetUserToken()
+		{
+			var userToken = await Configuration.Requester.Request(this.App.Host, "auth/session/userkey", new [] {
+				new KeyValuePair<string, object>("appSecret", this.App.Secret),
+				new KeyValuePair<string, object>("token", this.Token)
+			}, false);
+			if (userToken.SelectToken("error") != null) {
+				//Debug.WriteLine(userToken.ToString());
+				return null;
+			}
+			return new UserToken(userToken.Value<string>("accessToken"), userToken["user"].ToObject<JObject>());
+		}
+
+		public async Task<Account> WaitForAuth()
+		{
+			UserToken token;
+			while (true)
+			{
+				token = await this.GetUserToken();
+				if (token != null) break;
+				await Task.Delay(2000);
+			}
+			return new Account(this.App, token.AccessToken);
+		}
+	}
+
+	public class Account
+	{
+		public App App;
+		public string AccessToken;
+		private string i;
+
+		public Account(App app, string accessToken)
+		{
+			string calcAccessToken(string appSecret, string userToken)
+			{
+				using (var sha256 = SHA256.Create())
+				{
+					var tokenCodeBytes = Encoding.ASCII.GetBytes($"{userToken}{appSecret}");
+					var hashBytes = sha256.ComputeHash(tokenCodeBytes);
+					return string.Join("", hashBytes.Select(x => $"{x:x2}"));
+				}
+			}
+
+			this.App = app;
+			this.i = calcAccessToken(app.Secret, accessToken);
+		}
+
+		public Task<JToken> Request(string endpoint, IEnumerable<KeyValuePair<string, object>> parameters)
+		{
+			var baseParam = new [] {
+				new KeyValuePair<string, object>("i", this.i)
+			};
+			return Configuration.Requester.Request(this.App.Host, endpoint, baseParam.Concat(parameters), false);
 		}
 	}
 }
